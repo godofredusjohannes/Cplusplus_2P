2f006abce0d62c86eb8dfe2d9fbaefa2c159a405
serweryn.poczynek <bobbyfngaming@gmail.com>
2025-05-12 13:25:26 +0200

    Aktualizacja

--- cmake-build-debug/CMakeFiles/untitled2.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/untitled2.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/serweryn.poczynek/CLionProjects/PodstawyProgramowania2P/wisielec.cpp" "CMakeFiles/untitled2.dir/wisielec.cpp.o" "gcc" "CMakeFiles/untitled2.dir/wisielec.cpp.o.d"
+  "/Users/serweryn.poczynek/CLionProjects/PodstawyProgramowania2P/sortowanie_przez_wstawianie.cpp" "CMakeFiles/untitled2.dir/sortowanie_przez_wstawianie.cpp.o" "gcc" "CMakeFiles/untitled2.dir/sortowanie_przez_wstawianie.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- cmake-build-debug/CMakeFiles/untitled2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/untitled2.dir/cmake_clean.cmake
@@ -1,6 +1,6 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/untitled2.dir/wisielec.cpp.o"
-  "CMakeFiles/untitled2.dir/wisielec.cpp.o.d"
+  "CMakeFiles/untitled2.dir/sortowanie_przez_wstawianie.cpp.o"
+  "CMakeFiles/untitled2.dir/sortowanie_przez_wstawianie.cpp.o.d"
   "untitled2"
   "untitled2.pdb"
 )
